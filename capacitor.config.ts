16e6f22054902acd4ab147ffb7a39470a3a7d1d3
Eddie Ash <edwinashie289@outlook.com>
2021-11-08 15:28:53 +0000

    app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ionic-vue-todo',
+  appName: 'ionic-vue-memories',
   webDir: 'dist',
   bundledWebRuntime: false
 };
